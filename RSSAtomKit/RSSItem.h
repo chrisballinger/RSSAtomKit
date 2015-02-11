01c6ffd817e259bbf9a264797dc45115c5687282
David Chiles <dwalterc@gmail.com>
2015-02-11 13:13:37 -0800

    Added basic doc for RSSItem

--- RSSAtomKit/RSSItem.h
+++ RSSAtomKit/RSSItem.h
@@ -12,10 +12,11 @@
 
 @class RSSPerson;
 
-@import CoreGraphics.CGGeometry;
-
+/**
+ `RSSItem` base object of an item in a RSS or Atom feed. This is normally the nodes with tags 'item' or 'entry'.
+*/
 @interface RSSItem : MTLModel
-    
+
 @property (nonatomic, readonly) RSSFeedType feedType;
 
 @property (nonatomic, strong, readonly) NSString *title;
@@ -30,8 +31,21 @@
 
 @property (nonatomic, strong, readonly) NSArray *mediaItems;
 
+/**
+ Creates an RSSItemObject from an ONOXMLElemnt with a feed type hint
+ 
+ @param xmlElement the xmlElement that all the properties will be derived from
+ @param feedtype a hint at what type of feed the xml element is from
+ @return an initialized RSSItem
+ */
 - (instancetype) initWithFeedType:(RSSFeedType)feedType xmlElement:(ONOXMLElement*)xmlElement;
 
+/**
+ Creates an array of RSSItem(s) from an entire xml Document. For RSS it looks at all items that match the path /rss/channel/item and for Atom it looks at /feed/entry
+ 
+ @param feedType the type of feed
+ @return xmlDocumetn the docuement to be parsed
+ */
 + (NSArray*) itemsWithFeedType:(RSSFeedType)feedType xmlDocument:(ONOXMLDocument*)xmlDocument;
 
 @end
